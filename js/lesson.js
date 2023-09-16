2fcb88ee4318b817861f08aa81bae055f2638f1d
AbakarovMustafa-IT <webit2802@gmail.com>
2023-09-16 07:21:04 +0600

    project_program_DZ
    project_program_DZ

--- js/lesson.js
+++ js/lesson.js
@@ -19,3 +19,47 @@ phoneButton.onclick = () => {
     phoneResult.style.color = "red";
   }
 };
+
+// TAB SLIDER
+
+const tabContentBlocks = document.querySelectorAll(".tab_content_block");
+const tabItems = document.querySelectorAll(".tab_content_item");
+const parentTabs = document.querySelector(".tab_content_items");
+
+let currentIndex = 0;
+
+const hideTabContent = () => {
+  tabContentBlocks.forEach((tabContentBlock) => {
+    tabContentBlock.style.display = "none";
+  });
+  tabItems.forEach((tabItem) => {
+    tabItem.classList.remove("tab_content_item_active");
+  });
+};
+
+const showTabContent = (indexElement = 0) => {
+  tabContentBlocks[indexElement].style.display = "block";
+  tabItems[indexElement].classList.add("tab_content_item_active");
+};
+
+function autoSlide() {
+  hideTabContent();
+  currentIndex = (currentIndex + 1) % tabContentBlocks.length;
+  showTabContent(currentIndex);
+}
+
+const slideInterval = setInterval(autoSlide, 3000);
+
+parentTabs.onclick = (event) => {
+  if (event.target.classList.contains("tab_content_item")) {
+    tabItems.forEach((tabItem, tabIndex) => {
+      if (event.target === tabItem) {
+        clearInterval(slideInterval);
+        hideTabContent();
+        currentIndex = tabIndex;
+        showTabContent(tabIndex);
+        slideInterval = setInterval(autoSlide, 3000);
+      }
+    });
+  }
+};
---
+++ js/modal.js
@@ -0,0 +1,42 @@
+// MODAL
+
+const modal = document.querySelector(".modal");
+const closeModalButton = document.querySelector(".modal_close");
+const modalTrigger = document.querySelector("#btn-get");
+
+const openModal = () => {
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+  modal.style.display = "none";
+  document.body.style.overflow = "";
+};
+
+modalTrigger.onclick = () => openModal();
+closeModalButton.onclick = () => closeModal();
+modal.onclick = (event) => {
+  if (event.target === modal) {
+    closeModal();
+  }
+};
+
+function checkScrollAndShowModal() {
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.documentElement.scrollHeight;
+  const scrollPosition = window.scrollY;
+
+  // Проверяем, достиг ли пользователь конца страницы
+  if (scrollPosition + windowHeight >= documentHeight) {
+    openModal(); // Вызываем модальное окно
+
+    // Удаляем обработчик события после первого вызова
+    window.removeEventListener("scroll", checkScrollAndShowModal);
+  }
+}
+
+// Добавляем обработчик события scroll
+window.addEventListener("scroll", checkScrollAndShowModal);
+
+window.onload = () => setTimeout(openModal, 10000);
