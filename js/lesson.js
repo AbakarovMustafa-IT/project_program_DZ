9e2961f7877c2cd31cbdd463e469a87d04968724
AbakarovMustafa-IT <webit2802@gmail.com>
2023-09-27 13:23:02 +0600

    project_program_DZ
    project_program_DZ

--- js/lesson.js
+++ js/lesson.js
@@ -158,3 +158,44 @@ function convertAll(event) {
     });
   };
 }
+
+// CARD SWITCHER
+
+const card = document.querySelector(".card");
+const btnNext = document.querySelector("#btn-next");
+const btnPrev = document.querySelector("#btn-prev");
+
+let count = 1;
+
+const changeColor = (item) => {
+  item.completed
+    ? (card.style.background = "green")
+    : (card.style.background = "red");
+};
+
+const getTodos = () => {
+  fetch(`https://jsonplaceholder.typicode.com/todos?_limit=1&_page=${count}`)
+    .then((response) => response.json())
+    .then((data) => {
+      data?.forEach((item) => {
+        card.innerHTML = `
+                <h3>${item?.id}</h3>
+                <h4>${item?.title}</h4>
+                <p>${item?.completed}</p>
+                `;
+        changeColor(item);
+      });
+    });
+};
+
+getTodos();
+
+btnNext.onclick = () => {
+  count > 200 ? (count = 1) : count++;
+  getTodos();
+};
+
+btnPrev.onclick = () => {
+  count < 1 ? (count = 200) : count--;
+  getTodos();
+};
