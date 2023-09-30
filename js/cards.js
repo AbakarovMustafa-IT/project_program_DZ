584d5ec1190dd27280edae7e1069899f999c129f
AbakarovMustafa-IT <webit2802@gmail.com>
2023-09-30 17:55:24 +0600

    project_program_DZ
    project_program_DZ

--- js/cards.js
+++ js/cards.js
@@ -3,18 +3,30 @@ const cardsUser = document.querySelector(".cards");
 async function showCards() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-
     const data = await response.json();
-    cardsUser.innerHTML = `
-      <div class="cards">
-        <div class"card">
-          <img src="../images/User.jpg">
-          <h2>${data?.title}</h2>
-          <p>${data?.body}</p>
+
+    // Ограничиваем данные до 50 записей
+    const limitedData = data.slice(0, 50);
+
+    // Создаем HTML для карточек
+    const cardHTML = limitedData
+      .map(
+        (item) => `
+        <div class="inner_card_blog">
+        <div class="card_photo">
+          <img  class="card_photo_size" src="../images/User.jpg">
         </div>
-      </div>
-    `;
-  } catch (error) {}
+          <h2 class="card_title">${item.title}</h2>
+          <p class="card_descr">${item.body}</p>
+        </div>
+      `
+      )
+      .join(""); // Объединяем HTML в строку
+
+    cardsUser.innerHTML = cardHTML;
+  } catch (error) {
+    console.error("Произошла ошибка:", error);
+  }
 }
 
 showCards();
